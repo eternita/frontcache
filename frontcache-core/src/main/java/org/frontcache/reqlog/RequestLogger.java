56c457fe906e860b96f4a0eac296a333f3319800
Serhiy Pavlikovskiy <pavlikovskiy@gmail.com>
2015-11-08 20:25:02 -0700

    make request logger configurable

--- frontcache-core/src/main/java/org/frontcache/reqlog/RequestLogger.java
+++ frontcache-core/src/main/java/org/frontcache/reqlog/RequestLogger.java
@@ -1,7 +1,13 @@
 package org.frontcache.reqlog;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.frontcache.FCConfig;
 
 /**
  *  Log requests to file (log4j2) for statistics and further analysis
@@ -12,9 +18,36 @@ import org.apache.logging.log4j.Logger;
  */
 public class RequestLogger {
 
-	private static String SEPARATOR = " ";
+	private final static String SEPARATOR = " ";
+	
+	private final static String CONFIG_FILE_KEY = "request_logs_config";
+	
+	private final static String DEFAULT_CONFIG_FILE = "fc-log-config-default.xml";
 
-	private static Logger logger = LogManager.getLogger(RequestLogger.class.getName());
+	private static Logger logger = null;
+
+	static {
+		
+		String configFileName = FCConfig.getProperty(CONFIG_FILE_KEY);
+		
+		if (null == configFileName)
+			configFileName = DEFAULT_CONFIG_FILE;
+		
+		InputStream reqLogConfigIS = RequestLogger.class.getClassLoader().getResourceAsStream(configFileName);
+		if (null != reqLogConfigIS)
+		{
+			ConfigurationSource source;
+			try {
+				source = new ConfigurationSource(reqLogConfigIS);
+		        Configurator.initialize(null, source);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+        
+		logger = LogManager.getLogger(RequestLogger.class.getName());
+	}
+	
 
 	private RequestLogger() {
 	}
