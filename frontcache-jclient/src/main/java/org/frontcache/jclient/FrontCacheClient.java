c5680c0a1f3747242001d1c00dfd14e3067ebbb3
Serhiy Pavlikovskiy <pavlikovskiy@gmail.com>
2016-01-02 17:31:12 -0700

    add jclient stubs

---
+++ frontcache-jclient/src/main/java/org/frontcache/jclient/FrontCacheClient.java
@@ -0,0 +1,22 @@
+package org.frontcache.jclient;
+
+/**
+ * 
+ * callbacks for invalidation
+ *
+ */
+public class FrontCacheClient {
+
+
+	private FrontCacheClient() {
+	}
+
+	public static void remove(String filter)
+	{		
+	}
+	
+	public static void removeAll()
+	{		
+	}
+	
+}
