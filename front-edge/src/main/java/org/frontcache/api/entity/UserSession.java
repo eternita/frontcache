16c1e6a611683ebfba12692e4bead3fdc67c2055
dpa <dpa@192.168.2.7>
2016-01-20 00:09:20 -0700

    init commit of edge module

---
+++ front-edge/src/main/java/org/frontcache/api/entity/UserSession.java
@@ -0,0 +1,14 @@
+package org.frontcache.api.entity;
+
+public class UserSession {
+
+	public String getUserName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getUserId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
