d5a029de0984ee1f718ab4da0db7e2954e755847
Serhiy Pavlikovskiy <pavlikovskiy@gmail.com>
2016-05-15 16:48:29 -0600

    getKeys ++

--- frontcache-core/src/main/java/org/frontcache/io/CachedKeysActionResponse.java
+++ frontcache-core/src/main/java/org/frontcache/io/CachedKeysActionResponse.java
@@ -4,12 +4,24 @@ import java.util.List;
 
 public class CachedKeysActionResponse extends ActionResponse {
 
+	private long amount;
+	
 	private List<String> cachedKeys;
 	
 	public CachedKeysActionResponse(List<String> cachedKeys) {
 		setAction("cached keys");
 		setResponseStatus(RESPONSE_STATUS_OK);
 		this.cachedKeys = cachedKeys;
+		if (null != cachedKeys)
+			this.amount = cachedKeys.size();
+	}
+
+	public long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(long amount) {
+		this.amount = amount;
 	}
 
 	public List<String> getCachedKeys() {
