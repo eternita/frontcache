2b035b44bcda0b94c7f1b6821f28650793fad9c6
Serhiy Pavlikovskiy <pavlikovskiy@gmail.com>
2016-05-29 20:01:04 -0600

    frontcache-console - read config from system property #64

--- frontcache-console/src/main/java/org/frontcache/console/service/FrontcacheService.java
+++ frontcache-console/src/main/java/org/frontcache/console/service/FrontcacheService.java
@@ -1,13 +1,17 @@
 package org.frontcache.console.service;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.frontcache.FCConfig;
 import org.frontcache.client.FrontCacheClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +23,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class FrontcacheService {
 
+	public static final String FRONTCACHE_CONSOLE_CONFIG_PATH_SYSTEM_KEY = "org.frontcache.console.config"; 
+	
 	private static final Logger logger = LoggerFactory.getLogger(FrontcacheService.class);
 	private ObjectMapper jsonMapper = new ObjectMapper();
 
@@ -44,7 +50,7 @@ public class FrontcacheService {
 			// data in frontcache-console.properties
 			//"https://or.coinshome.net:443/"
 			//"https://sg.coinshome.net:443/"
-			BufferedReader reader = new BufferedReader(new InputStreamReader(FrontcacheService.class.getClassLoader().getResourceAsStream("frontcache-console.properties")));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(getConfigInputStream("frontcache-console.properties")));
 			 
 			while (true) {
 			    String line = reader.readLine();
@@ -61,6 +67,45 @@ public class FrontcacheService {
 		
 		return fcClients;
 	}
+	
+	public static InputStream getConfigInputStream(String name)
+	{
+		
+		InputStream is = null;
+		
+		// 1. get input stream from system variable frontcache.home
+		String frontcacheConsoleConfigPath = System.getProperty(FRONTCACHE_CONSOLE_CONFIG_PATH_SYSTEM_KEY);
+		
+		if (null != frontcacheConsoleConfigPath)
+		{
+			File frontcacheConsoleConfigFile = new File(frontcacheConsoleConfigPath);
+			if (frontcacheConsoleConfigFile.exists())
+			{
+				try {
+					is = new FileInputStream(frontcacheConsoleConfigFile);
+					logger.info(name + " is loaded from " + frontcacheConsoleConfigPath);
+				} catch (Exception e) {		}
+			}
+		}
+		
+		if (null != is)
+			return is;
+		
+		
+		// 2. get input stream from inside jars (/frontcache-console.properties)
+		try 
+		{
+			is = FrontcacheService.class.getClassLoader().getResourceAsStream(name);
+			logger.info(name + " is loaded from class path");
+		} catch (Exception e) {		}
+		
+		
+    	if (null == is)
+    		throw new RuntimeException("Can't load " + name + " from classpath and " + FRONTCACHE_CONSOLE_CONFIG_PATH_SYSTEM_KEY + " (java system variable) ");
+		
+		return is;
+	}
+	
 	
 	public String getHystrixMonitorURLList()
 	{
