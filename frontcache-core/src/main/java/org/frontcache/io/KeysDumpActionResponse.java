fccb674d105be0b56b7d729a3f356309ff8d383a
Serhiy Pavlikovskiy <pavlikovskiy@gmail.com>
2016-06-24 23:11:23 -0600

    FrontcacheIO - keys-dump and reload ++

--- frontcache-core/src/main/java/org/frontcache/io/KeysDumpActionResponse.java
+++ frontcache-core/src/main/java/org/frontcache/io/KeysDumpActionResponse.java
@@ -1,17 +1,11 @@
 package org.frontcache.io;
 
-import java.util.Map;
-
 public class KeysDumpActionResponse extends ActionResponse {
 
 	
 	public KeysDumpActionResponse() { // for JSON mapper
-		
-	}
-	
-	public KeysDumpActionResponse(Map<String, String> cacheStatus) {
 		setAction("key dump started - will be saved to ./warmer dir");
 		setResponseStatus(RESPONSE_STATUS_OK);
 	}
-
+	
 }
--- frontcache-core/src/main/java/org/frontcache/io/ReloadActionResponse.java
+++ frontcache-core/src/main/java/org/frontcache/io/ReloadActionResponse.java
@@ -1,17 +1,11 @@
 package org.frontcache.io;
 
-import java.util.Map;
-
 public class ReloadActionResponse extends ActionResponse {
 
 	
 	public ReloadActionResponse() { // for JSON mapper
-		
-	}
-	
-	public ReloadActionResponse(Map<String, String> cacheStatus) {
 		setAction("Frontcache edge has been reloaded");
 		setResponseStatus(RESPONSE_STATUS_OK);
 	}
-
+	
 }
