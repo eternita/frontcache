f9ee6e9c1e01d05e85bc4b4eae5b03938b25a9b6
Serhiy Pavlikovskiy <pavlikovskiy@gmail.com>
2016-09-19 21:52:27 -0600

    warmer - start from specified position

--- warmer/warmer.sh
+++ warmer/warmer.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# put line number to start from
+start_line=0
+
+counter=0
+
 while IFS='' read -r line || [[ -n "$line" ]]; do
- echo "crawling $line"
- curl -H "Accept: text/html" -H "Accept-Encoding: gzip, deflate" -H "user-agent: Googlebot" -o output.log $line
+ 
+  counter=$((counter+1))
+  if [ $counter -gt $start_line ]
+  then
+    echo "crawling $line"
+    curl -H "Accept: text/html" -H "Accept-Encoding: gzip, deflate" -H "user-agent: Googlebot" -o output.log $line
+  fi 
+  
 done < "$1"
