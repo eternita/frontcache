5e517c2c00c753ec1005672c83a61e5bb6309e26
Serhiy Pavlikovskiy <pavlikovskiy@gmail.com>
2016-10-04 12:51:22 -0600

    console: sort in alfabetic order

--- frontcache-console/src/main/java/org/frontcache/console/service/FrontcacheService.java
+++ frontcache-console/src/main/java/org/frontcache/console/service/FrontcacheService.java
@@ -7,7 +7,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -30,7 +30,7 @@ public class FrontcacheService {
 	private static final Logger logger = LoggerFactory.getLogger(FrontcacheService.class);
 	private ObjectMapper jsonMapper = new ObjectMapper();
 	
-	private Set<String> frontcacheAgentURLs = new HashSet<String>();
+	private Set<String> frontcacheAgentURLs = new LinkedHashSet<String>();
 	
 	public FrontcacheService() {
 		loadConfigs();
