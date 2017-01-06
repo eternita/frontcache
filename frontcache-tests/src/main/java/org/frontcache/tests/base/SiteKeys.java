95dea05095478b2bdb8faaee8354175480a866c0
Serhiy Pavlikovskiy <pavlikovskiy@gmail.com>
2017-01-05 17:00:01 -0700

    tests works

--- frontcache-tests/src/main/java/org/frontcache/tests/base/SiteKeys.java
+++ frontcache-tests/src/main/java/org/frontcache/tests/base/SiteKeys.java
@@ -1,6 +1,7 @@
 package org.frontcache.tests.base;
 
 public interface SiteKeys {
+	public static final String TEST_SITE_KEY_LOCALHOST = "test-site-key-localhost";
 	public static final String TEST_SITE_KEY_1 = "test-site-key-1";
 	public static final String TEST_SITE_KEY_2 = "test-site-key-2";
 }
