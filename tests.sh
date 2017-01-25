42d735da130fb864b9d62cf11acfb118313bab67
Serhiy Pavlikovskiy <pavlikovskiy@gmail.com>
2017-01-25 09:40:24 -0700

    cleanup log and cache files before tests

--- tests.sh
+++ tests.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# cleanup cache dirs & logs
+# for filter 
+rm -rf ./frontcache-tests/FRONTCACHE_HOME_FILTER/cache/l2-lucene-index/*
+rm -rf ./frontcache-tests/FRONTCACHE_HOME_FILTER/logs/*
+
+# for standalone test server 
+rm -rf ./frontcache-tests/FRONTCACHE_HOME_STANDALONE/cache/l2-lucene-index/*
+rm -rf ./frontcache-tests/FRONTCACHE_HOME_STANDALONE/logs/*
+# end cleanup cache dirs & logs
+
+
 ./gradlew clean :frontcache-tests:startStandaloneFrontcache
 ./gradlew :frontcache-tests:end2endTests
 ./gradlew :frontcache-tests:stopStandaloneFrontcacheByPort
