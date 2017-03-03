bbb32bf687540df9e34958f2fcbf28645ccac1ca
dpavlikovskiy <pdaviti@gmail.com>
2017-03-02 20:47:08 -0700

    #211-clean logs after 10 days

--- scripts/docker/clean_logs.sh
+++ scripts/docker/clean_logs.sh
@@ -1,1 +1,4 @@
-curator --config /opt/curator/curator.yml  opt/curator/actions.yml
+#!/usr/bin/env bash
+
+CONTAINER_ID=`docker ps -a -f name=frontcache-logstash -q`
+docker exec $CONTAINER_ID  curator --config /opt/curator/curator.yml  opt/curator/actions.yml
