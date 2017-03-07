c6afd2a2061ae31ca38b228a11c4acca8b604ee2
dpavlikovskiy <pdaviti@gmail.com>
2017-03-07 09:54:11 -0700

    #211-use www and googlebot header

--- scripts/jmeter/runSiteMapTest.sh
+++ scripts/jmeter/runSiteMapTest.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./apache-jmeter-3.1/bin/jmeter -n -JTestHost=fc3.coinshome.net -JnThreads=2  -t ./scripts/SiteMapTest.jmx -l ./scripts/SitemapTestResults.jtl
+./apache-jmeter-3.1/bin/jmeter -n -JTestHost=www.coinshome.net -JnThreads=2  -t ./scripts/SiteMapTest.jmx -l ./scripts/SitemapTestResults.jtl
